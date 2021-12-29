2e00e0a8f44e4de11a073b6093d7b3c31ec2ef16
gianpaolo <g.laurenzano@studenti.unisa.it>
2021-12-29 20:37:43 +0100

    Changes Skill.java, SkillDipendente.java Documento.java

--- src/main/java/it/unisa/agency_formation/formazione/domain/Skill.java
+++ src/main/java/it/unisa/agency_formation/formazione/domain/Skill.java
@@ -4,4 +4,24 @@ public class Skill {
     private String nomeSkill;
     private String descrizioneSkill;
 
+    public Skill(){}
+    public Skill(String nomeSkill, String descrizioneSkill) {
+        this.nomeSkill = nomeSkill;
+    }
+
+    public String getNomeSkill() {
+        return nomeSkill;
+    }
+
+    public void setNomeSkill(String nomeSkill) {
+        this.nomeSkill = nomeSkill;
+    }
+
+    public String getDescrizioneSkill() {
+        return descrizioneSkill;
+    }
+
+    public void setDescrizioneSkill(String descrizioneSkill) {
+        this.descrizioneSkill = descrizioneSkill;
+    }
 }
