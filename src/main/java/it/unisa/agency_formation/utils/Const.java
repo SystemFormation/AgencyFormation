b5ebfcadc83fc098965771da73cf53baf0da059b
Genny <gennarocecco122@gmail.com>
2022-01-16 00:39:19 +0100

    Deleted Forlder DownloadFilesTest

--- src/main/java/it/unisa/agency_formation/utils/Const.java
+++ src/main/java/it/unisa/agency_formation/utils/Const.java
@@ -3,8 +3,8 @@ package it.unisa.agency_formation.utils;
 public class Const {
 
     public static String nomeDB = Const.NOME_DB_MANAGER;
-    public static final String NOME_DB_MANAGER = "af_db";
-    public static final String NOME_DB_TEST = "af_db_test";
+    public final static String NOME_DB_MANAGER = "af_db";
+    public final static String NOME_DB_TEST = "af_db_test";
     public static String name = "root";
-    public static String pwd = "root";
+    public static String pwd = "root11";
 }
