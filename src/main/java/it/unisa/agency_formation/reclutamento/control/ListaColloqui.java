7e7be7c880a237caec8594a88432a02e0301d2f0
Manuel Nocerino <manuel.nocerino@gmail.com>
2022-01-19 22:39:16 +0100

    ListaCandidatiTest.java 100% Branch, ListaColloquiTest.java 100% Branch

--- src/main/java/it/unisa/agency_formation/reclutamento/control/ListaColloqui.java
+++ src/main/java/it/unisa/agency_formation/reclutamento/control/ListaColloqui.java
@@ -36,7 +36,7 @@ public class ListaColloqui  extends HttpServlet {
         }
     }
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         doGet(req, resp);
     }
 
--- src/test/java/agency_formation/reclutamento/control/ListaCandidatiTest.java
+++ src/test/java/agency_formation/reclutamento/control/ListaCandidatiTest.java
@@ -4,6 +4,7 @@ import it.unisa.agency_formation.autenticazione.domain.RuoliUtenti;
 import it.unisa.agency_formation.autenticazione.domain.Utente;
 import it.unisa.agency_formation.autenticazione.manager.AutenticazioneManagerImpl;
 import it.unisa.agency_formation.reclutamento.control.ListaCandidati;
+import it.unisa.agency_formation.reclutamento.control.ListaColloqui;
 import it.unisa.agency_formation.reclutamento.control.ViewCandidaturaControl;
 import it.unisa.agency_formation.reclutamento.domain.Candidatura;
 import it.unisa.agency_formation.reclutamento.domain.StatiCandidatura;
@@ -38,8 +39,28 @@ public class ListaCandidatiTest {
     private ServletConfig config;
     private ServletContext context;
 
+    @Test //User null
+    public void userNull() throws ServletException, IOException {
+        Utente user = null;
+        config = Mockito.mock(ServletConfig.class);
+        request = Mockito.mock(HttpServletRequest.class);
+        response = Mockito.mock(HttpServletResponse.class);
+        session = Mockito.mock(HttpSession.class);
+        context = Mockito.mock(ServletContext.class);
+        ListaCandidati servlet = Mockito.spy(ListaCandidati.class);
+        Mockito.when(request.getSession()).thenReturn(session);
+        Mockito.when(session.getAttribute("user")).thenReturn(user);
+        Mockito.when(request.getServletContext()).thenReturn(context);
+        Mockito.when(context.getRequestDispatcher(anyString())).thenReturn(dispatcher);
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter writer = new PrintWriter(stringWriter);
+        Mockito.when(response.getWriter()).thenReturn(writer);
+        servlet.init(config);
+        servlet.doGet(request, response);
+        assertTrue(stringWriter.toString().contains("2"));
+    }
     @Test //User non HR
-    public void candidatiNull() throws ServletException, IOException {
+    public void userRoleFail() throws ServletException, IOException {
         Utente user = new Utente();
         user.setId(1);
         user.setRole(RuoliUtenti.CANDIDATO);
@@ -47,27 +68,21 @@ public class ListaCandidatiTest {
         user.setEmail("l.giacchetti@studenti.unisa.it");
         user.setName("Luigi");
         user.setSurname("Giacchetti");
-        ArrayList<Utente> List = null;
         config = Mockito.mock(ServletConfig.class);
         request = Mockito.mock(HttpServletRequest.class);
         response = Mockito.mock(HttpServletResponse.class);
         session = Mockito.mock(HttpSession.class);
-        dispatcher = Mockito.mock(RequestDispatcher.class);
         context = Mockito.mock(ServletContext.class);
         ListaCandidati servlet = Mockito.spy(ListaCandidati.class);
         Mockito.when(request.getSession()).thenReturn(session);
         Mockito.when(session.getAttribute("user")).thenReturn(user);
         Mockito.when(request.getServletContext()).thenReturn(context);
-        Mockito.when(context.getRequestDispatcher(anyString())).thenReturn(dispatcher);
-        try (MockedStatic mockedStatic = mockStatic(ListaCandidati.class)) {
-            mockedStatic.when(() -> ListaCandidati.getCandidatesFromManager()).thenReturn(List);
-            StringWriter stringWriter = new StringWriter();
-            PrintWriter writer = new PrintWriter(stringWriter);
-            Mockito.when(response.getWriter()).thenReturn(writer);
-            servlet.init(config);
-            servlet.doGet(request, response);
-            assertEquals("2", stringWriter.toString());
-        }
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter writer = new PrintWriter(stringWriter);
+        Mockito.when(response.getWriter()).thenReturn(writer);
+        servlet.init(config);
+        servlet.doGet(request, response);
+        assertTrue(stringWriter.toString().contains("2"));
     }
     @Test
     public void candidatiPass() throws ServletException, IOException, SQLException {
@@ -78,7 +93,15 @@ public class ListaCandidatiTest {
         user.setEmail("l.giacchetti@studenti.unisa.it");
         user.setName("Luigi");
         user.setSurname("Giacchetti");
-        ArrayList<Utente> List = null;
+        Utente user2 = new Utente();
+        user.setId(2);
+        user.setRole(RuoliUtenti.HR);
+        user.setPwd("lol");
+        user.setEmail("LuigiRossi@gmail.com");
+        user.setName("Luigi");
+        user.setSurname("Rossi");
+        ArrayList<Utente> list = new ArrayList<Utente>();
+        list.add(user2);
         config = Mockito.mock(ServletConfig.class);
         request = Mockito.mock(HttpServletRequest.class);
         response = Mockito.mock(HttpServletResponse.class);
@@ -91,13 +114,14 @@ public class ListaCandidatiTest {
         Mockito.when(request.getServletContext()).thenReturn(context);
         Mockito.when(context.getRequestDispatcher(anyString())).thenReturn(dispatcher);
         try (MockedStatic mockedStatic = mockStatic(ListaCandidati.class)) {
-            mockedStatic.when(() -> ListaCandidati.getCandidatesFromManager()).thenReturn(List);
+            mockedStatic.when(() -> ListaCandidati.getCandidatesFromManager()).thenReturn(list);
             StringWriter stringWriter = new StringWriter();
             PrintWriter writer = new PrintWriter(stringWriter);
             Mockito.when(response.getWriter()).thenReturn(writer);
             servlet.init(config);
             servlet.doGet(request, response);
-            assertEquals("1", stringWriter.toString());
+            writer.flush();
+            assertTrue(stringWriter.toString().contains("1"));
         }
     }
 }
