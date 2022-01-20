b688151e6d1c2acf3f1d05481bafa77de376c682
Manuel Nocerino <manuel.nocerino@gmail.com>
2022-01-20 23:17:46 +0100

    Fixing AddListaTeamTestIT.java

--- src/test/java/agency_formation/team/control/AddListaTeamTestIT.java
+++ src/test/java/agency_formation/team/control/AddListaTeamTestIT.java
@@ -42,86 +42,35 @@ public class AddListaTeamTestIT {
     public static void init() throws SQLException {
         Const.nomeDB = Const.NOME_DB_TEST;
         String insertTeam = "insert into team (idTeam,NomeProgetto,NumeroDipendenti,NomeTeam,Descrizione,Competenza,IdTM) values(5, 'ReqMem',8,'Inspiegabili','Non siamo eroi',null,3)";
+        String insertUtente = "Insert into Utenti (IdUtente,Nome,Cognome,Pwd,Mail,Ruolo) values(20,'test','test','test','test@gmail.com', 2)";
+        String insertDipendente = "insert into dipendenti (IdDipendente,Residenza, Telefono, Stato, AnnoDiNascita, IdTeam) values(20, 'Fisciano','77777',1,2000,5)";
+
         Connection connection = DatabaseManager.getInstance().getConnection();
         PreparedStatement statement = connection.prepareStatement(insertTeam);
-        statement.executeUpdate(insertTeam);
+        PreparedStatement statement2 = connection.prepareStatement(insertUtente);
+        PreparedStatement statement3 = connection.prepareStatement(insertDipendente);
+        statement.executeUpdate();
+        statement2.executeUpdate();
+        statement3.executeUpdate();
     }
 
     @AfterAll
     public static void finish() throws SQLException {
+        String deleteTeam = "delete from team where idTeam > 4";
+        String deleteUtente = "delete from Utenti where IdUtente > 19";
+        String deleteDipendente = "delete from dipendenti where idDipendente > 19";
+        Connection connection = DatabaseManager.getInstance().getConnection();
+
+        PreparedStatement statement = connection.prepareStatement(deleteTeam);
+        PreparedStatement statement2 = connection.prepareStatement(deleteUtente);
+        PreparedStatement statement3 = connection.prepareStatement(deleteDipendente);
+        statement.executeUpdate();
+        statement2.executeUpdate();
+        statement3.executeUpdate();
         Const.nomeDB = Const.NOME_DB_MANAGER;
     }
     @Test
-    public void userfail() throws IOException, ServletException {
-        request = Mockito.mock(HttpServletRequest.class);
-        response = Mockito.mock(HttpServletResponse.class);
-        session = Mockito.mock(HttpSession.class);
-        dispatcher = Mockito.mock(RequestDispatcher.class);
-        context = Mockito.mock(ServletContext.class);
-        Mockito.when(request.getSession()).thenReturn(session);
-        Mockito.when(session.getAttribute("user")).thenReturn(null);
-        Mockito.when(request.getSession(true)).thenReturn(session);
-        Mockito.when(request.getServletContext()).thenReturn(context);
-        StringWriter stringWriter = new StringWriter();
-        PrintWriter writer = new PrintWriter(stringWriter);
-        Mockito.when(response.getWriter()).thenReturn(writer);
-        new AddTeamControl().doPost(request, response);
-        writer.flush();
-        assertTrue(stringWriter.toString().contains("5"));
-    }
-    @Test
-    public void rolefail() throws IOException, ServletException {
-        int idUser = 10;
-        Utente user = new Utente("Mario", "Rossi", "mario.rossi@gmail.com", "123", RuoliUtenti.CANDIDATO);
-        user.setId(idUser);
-        request = Mockito.mock(HttpServletRequest.class);
-        response = Mockito.mock(HttpServletResponse.class);
-        session = Mockito.mock(HttpSession.class);
-        dispatcher = Mockito.mock(RequestDispatcher.class);
-        context = Mockito.mock(ServletContext.class);
-        Mockito.when(request.getSession()).thenReturn(session);
-        Mockito.when(session.getAttribute("user")).thenReturn(user);
-        Mockito.when(request.getSession(true)).thenReturn(session);
-        Mockito.when(request.getServletContext()).thenReturn(context);
-        StringWriter stringWriter = new StringWriter();
-        PrintWriter writer = new PrintWriter(stringWriter);
-        Mockito.when(response.getWriter()).thenReturn(writer);
-        new AddTeamControl().doPost(request, response);
-        writer.flush();
-        assertTrue(stringWriter.toString().contains("5"));
-    }
-    @Test
-    public void addTeamIdDipMoreThan0() throws IOException, ServletException {
-        int idDip = -1;
-        int idTeam = 01;
-        Utente user = new Utente("Mario", "Rossi", "mario.rossi@gmail.com", "123", RuoliUtenti.TM);
-        user.setId(idDip);
-        config = Mockito.mock(ServletConfig.class);
-        request = Mockito.mock(HttpServletRequest.class);
-        response = Mockito.mock(HttpServletResponse.class);
-        session = Mockito.mock(HttpSession.class);
-        dispatcher = Mockito.mock(RequestDispatcher.class);
-        context = Mockito.mock(ServletContext.class);
-        String action = "aggiungi";
-
-        AddTeamControl servlet = Mockito.spy(AddTeamControl.class);
-        Mockito.when(request.getSession()).thenReturn(session);
-        Mockito.when(session.getAttribute("user")).thenReturn(user);
-
-        Mockito.when(request.getServletContext()).thenReturn(context);
-        Mockito.when(request.getParameter("action")).thenReturn(action);
-        Mockito.when(request.getParameter("id")).thenReturn(String.valueOf(idDip));
-
-        StringWriter stringWriter = new StringWriter();
-        PrintWriter writer = new PrintWriter(stringWriter);
-        Mockito.when(response.getWriter()).thenReturn(writer);
-        servlet.init(config);
-        servlet.doGet(request, response);
-        writer.flush();
-        assertTrue(stringWriter.toString().contains("3"));
-    }
-    @Test
-    public void addTeamPass() throws IOException, ServletException {
+    public void addTeamPass() throws IOException, ServletException, SQLException {
         int idDip = 10;
         int idTeam = 5;
         Utente user = new Utente("Mario", "Rossi", "mario.rossi@gmail.com", "123", RuoliUtenti.TM);
@@ -140,9 +89,8 @@ public class AddListaTeamTestIT {
 
         Mockito.when(request.getServletContext()).thenReturn(context);
         Mockito.when(request.getParameter("action")).thenReturn(action);
-        Mockito.when(request.getParameter("id")).thenReturn(String.valueOf(idDip));
+        Mockito.when(request.getParameter("id")).thenReturn(String.valueOf(20));
         Mockito.when(request.getParameter("idTeam")).thenReturn(String.valueOf(idTeam));
-
 
         Mockito.when(context.getRequestDispatcher(anyString())).thenReturn(dispatcher);
         StringWriter stringWriter = new StringWriter();
@@ -152,6 +100,37 @@ public class AddListaTeamTestIT {
         servlet.doGet(request, response);
         writer.flush();
         assertTrue(stringWriter.toString().contains("2"));
+
         }
-    }
+    @Test
+    public void addTeamFail() throws IOException, ServletException {
+        int idDip = 10;
+        int idTeam = 2;
+        Utente user = new Utente("Mario", "Rossi", "mario.rossi@gmail.com", "123", RuoliUtenti.TM);
+        user.setId(10);
+        config = Mockito.mock(ServletConfig.class);
+        request = Mockito.mock(HttpServletRequest.class);
+        response = Mockito.mock(HttpServletResponse.class);
+        session = Mockito.mock(HttpSession.class);
+        dispatcher = Mockito.mock(RequestDispatcher.class);
+        context = Mockito.mock(ServletContext.class);
+        String action = "aggiungi";
+
+        AddTeamControl servlet = Mockito.spy(AddTeamControl.class);
+        Mockito.when(request.getSession()).thenReturn(session);
+        Mockito.when(session.getAttribute("user")).thenReturn(user);
+
+        Mockito.when(request.getServletContext()).thenReturn(context);
+        Mockito.when(request.getParameter("action")).thenReturn(action);
+        Mockito.when(request.getParameter("id")).thenReturn(String.valueOf(1));
+        Mockito.when(request.getParameter("idTeam")).thenReturn(String.valueOf(idTeam));
+
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter writer = new PrintWriter(stringWriter);
+        Mockito.when(response.getWriter()).thenReturn(writer);
+        servlet.init(config);
+        servlet.doGet(request, response);
+        writer.flush();
+        assertTrue(stringWriter.toString().contains("1"));
+        }
 }
